8f948c66bcebf8e78f25455ff3719cba156415e8
JibrilExe <cedric00ik@gmail.com>
2026-04-19 16:22:58 +0200

    feat: code seperation

--- backend/car_backend.py
+++ backend/car_backend.py
@@ -10,9 +10,8 @@ import uuid
 import os
 import psycopg2
 from flask_cors import CORS
-import replicate
-import requests
 import time
+from helpers import remove_background
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 app = Flask(__name__, static_url_path='/static')
@@ -54,36 +53,6 @@ def init_db():
     conn.commit()
 
 init_db()
-
-def remove_background(image_path: str, output_path: str):
-    """
-    Takes a local file path, runs AI background removal,
-    saves result to output_path, returns output_path.
-    """
-
-    # Upload local file as file-like object
-    with open(image_path, "rb") as file:
-        output = replicate.run(
-            "cjwbw/rembg:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003",
-            input={
-                "image": file
-            }
-        )
-
-    # replicate returns a URL or file-like object depending on model
-    # safest way: handle both cases
-
-    if hasattr(output, "read"):
-        # file-like
-        with open(output_path, "wb") as f:
-            f.write(output.read())
-    else:
-        # URL case
-        r = requests.get(output.url)
-        with open(output_path, "wb") as f:
-            f.write(r.content)
-
-    return output_path
 
 @app.route("/upload", methods=["POST"])
 def upload():
